1d9089f4c6e5e04f9fd9c8dc1b0a9da66160a8dc
Rehman <sherryrehman250@gmail.com>
2026-06-26 01:25:38 +0500

    Add encode and decode methods for strings
    Implement encoding and decoding for a list of strings.

---
+++ Encode and Decode Strings.cpp
@@ -0,0 +1,37 @@
+Time Complexity
+
+encode: O(total characters)
+decode: O(total characters)
+
+Space Complexity
+
+O(total characters) for the output.
+
+
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res;
+        for(string &s : strs){
+            res += to_string(s.size()) + '#' + s;
+        }    
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>res;
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j] !='#'){
+                j++;
+            }
+            int len=stoi(s.substr(i,j-i));
+            string word = s.substr(j+1,len);
+            res.push_back(word);
+            i=j+1+len;
+        }
+       return res;
+    }
+};
